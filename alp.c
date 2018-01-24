5703d7424229b489dd629e8d0ffcdc24850f71a6
sktperiasamy <31505529+sktperiasamy@users.noreply.github.com>
2018-01-24 22:20:38 +0530

    Create alp.c

---
+++ alp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    char c;
+    printf("Enter a character or word: ");
+    scanf("%c",&c);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf("%c is an alphabet..",c);
+    else
+        printf("%c is not an alphabet..",c);
+
+    return 0;
+}
