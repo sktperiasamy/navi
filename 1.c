6154a6219833cad3e42ca5e726672a98b8c3cb24
sktperiasamy <31505529+sktperiasamy@users.noreply.github.com>
2018-02-05 16:40:58 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,p=1,d,sum=0,a;
+  clrscr();
+	printf("Enter n and d values");
+	scanf("%d %d ",&n,&d);
+	for(a=1;a<=n;a++)
+	{
+		sum=sum+p;
+		p=p+d;
+	}
+	printf("\n%d",sum);
+getch();
+}
+
